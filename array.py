524fa11bffeaf689fad4ff61c2179a1103add957
rrismaay0-yey <rrismaay0@gmail.com>
2025-11-10 16:13:01 +0800

    Create array.py

---
+++ array.py
@@ -0,0 +1,20 @@
+nama : nurrisma
+kelas : B/sitem informasi 
+nim : D0425308
+
+# Array
+buah = ["apel", "mangga", "jeruk", "strawberry"]
+
+# Mengakses elemen
+print(buah[1] = buah)
+
+# Menambah elemen
+buah.append("semangka")
+
+# Mengubah elemen
+buah[2] = "anggur"
+
+# Menghapus elemen
+buah.remove("strawberry")
+
+print("Daftar buah:", buah)
