5e4da335f967e3e4bd15f22920a4aefab7c46d15
rrismaay0-yey <rrismaay0@gmail.com>
2025-11-28 15:28:41 +0800

    Add script to calculate area of a rectangle

---
+++ Persegi.py
@@ -0,0 +1,10 @@
+#Nurrisma
+#D0425308
+#Sistem informasi B
+
+#LUAS PERSEGI 
+sisi = int(input(“Masukkan sisi pertama: ”))
+sisikedua = int(input(“Masukkan sisi kedua: ”))
+Luas = int(sisi * sisikedua)
+
+print('luas persegi adalah: ' , luas)
